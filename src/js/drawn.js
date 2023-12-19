d1c280aca181eec4cca5a2e69da0506064581ce6
Simon Højberg <r.hackr@gmail.com>
2023-12-19 13:58:17 -0500

    Update with paper

--- src/js/drawn.js
+++ src/js/drawn.js
@@ -2,30 +2,31 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Timeline } from "gsap/gsap-core";
 
+const one = document.querySelector.bind(document);
+const all = document.querySelectorAll.bind(document);
+
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Timeline);
 
 // ~*~*~*~*~*~*~*~ SKY ELEMENTS SCROLL TIMELINE ~*~*~*~*~*~*~*~
 
-let skyTL = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".cloud-container",
-    start: "center center",
-    end: "bottom top",
-    ease: "ease-in",
-    scrub: 3,
-    pin: true,
-    // markers: true,
-    stagger: 1.5,
-  },
-});
-skyTL
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".cloud-container",
+      start: "center center",
+      end: "bottom top",
+      ease: "ease-in",
+      scrub: 3,
+      pin: true,
+      // markers: true,
+      stagger: 1.5,
+    },
+  })
   .from("#moon-1", { y: innerHeight * 1.2, x: innerWidth * 0.2 })
   .from("#cloud-1", { y: innerHeight * 1.1, x: innerWidth * 0.3 })
   .from("#cloud-2", { y: innerHeight * 1.0, x: 50 })
   .from("#santa-1", { y: innerHeight * 1.3, x: -innerWidth, duration: 4 })
-  .from("#aoc-heading", { y: innerHeight * 1.1, duration: 2 })
-  .from("#aoc-heading-2", { y: innerHeight * 1.1 })
   .to({}, { duration: 2 });
 
 // ~*~*~*~*~*~*~*~ GROUND ELEMENTS SCROLL TIMELINE ~*~*~*~*~*~*~*~
@@ -41,8 +42,9 @@ const groundScroll = {
   // pin: true,
   pinType: "fixed",
 };
-let groundTL = gsap.timeline({ scrollTrigger: groundScroll });
-groundTL
+
+gsap
+  .timeline({ scrollTrigger: groundScroll })
   .from(".stars", { y: innerHeight * 0.2 })
   .from(".left-front-tree", { y: innerHeight * 0.3 }, 0.1)
   .from(".right-front-tree", { y: innerHeight * 0.3 }, "<")
@@ -54,8 +56,7 @@ groundTL
   .from(".lt-bkground", { y: innerHeight * 0.3 }, 0.2)
   .from(".left-foreground", { y: innerHeight * 0.2 }, 0.1)
   .from(".right-foreground", { y: innerHeight * 0.2 }, "<")
-  .from(".present-back", { y: innerHeight * 0.25 }, 2)
-  .from(".present-primary", { y: innerHeight * 0.5 }, "<");
+  .from(".present-back", { y: innerHeight * 0.25 }, 2);
 
 // ~*~*~*~*~*~*~*~ SLOWER SCROLL TIMELINE ~*~*~*~*~*~*~*~
 
@@ -68,8 +69,9 @@ let slowScroll = {
   scrub: 3,
   anticipatePin: 1,
 };
-let slowTL = gsap.timeline({ scrollTrigger: slowScroll });
-slowTL
+
+gsap
+  .timeline({ scrollTrigger: slowScroll })
   .fromTo(
     ".comet",
     { y: -300, x: innerWidth * 1.1 },
@@ -77,17 +79,23 @@ slowTL
   )
   .from(".main-tree", { y: innerHeight * 0.85 }, 0.5);
 
+let verySlow = {
+  trigger: ".ground-container",
+  start: "-=1000",
+  end: "bottom bottom",
+  normalizeScroll: true,
+  ease: "ease-in",
+  scrub: 3,
+  anticipatePin: 1,
+};
+
+gsap
+  .timeline({ scrollTrigger: verySlow })
+  .from(".card-container", { y: innerHeight * 0.85 }, 0.5);
+
 // ~*~*~*~*~*~*~*~ NON SCROLL ANIMATIONS ~*~*~*~*~*~*~*~
 
-let primaryPresentWiggle = gsap.to(".present-primary", {
-  repeat: -1,
-  duration: 2,
-  ease: "bounce.out",
-  yoyo: true,
-  rotate: 2,
-  transformOrigin: "60% 90%",
-  scale: 0.95,
-});
+/*
 let leftForegroundAnim = gsap.fromTo(
   ".left-foreground",
   {
@@ -103,6 +111,7 @@ let leftForegroundAnim = gsap.fromTo(
     duration: 2.1,
   },
 );
+
 let rightForegroundAnim = gsap.fromTo(
   ".right-foreground",
   {
@@ -118,7 +127,11 @@ let rightForegroundAnim = gsap.fromTo(
     duration: 2,
   },
 );
+
+*/
+
 let lights = gsap.utils.toArray(".lights");
+
 lights.forEach((light) => {
   gsap.to(light, {
     opacity: 1,
@@ -126,53 +139,4 @@ lights.forEach((light) => {
     repeat: -1,
     yoyo: true,
   });
-});
-let present = gsap
-  .to(".present-primary", {
-    duration: 1,
-    ease: "ease-in",
-    rotate: 4,
-    transformOrigin: "60% 90%",
-    scale: 1.55,
-  })
-  .reverse();
-let card = gsap
-  .to(".card-container", {
-    duration: 1,
-    delay: 1,
-    ease: "ease-in",
-    opacity: 1,
-  })
-  .reverse();
-
-//  ~*~*~*~*~*~*~*~ SCROLL EVENT LISTENERS ~*~*~*~*~*~*~*~
-
-function pauseAnimations() {
-  primaryPresentWiggle.pause();
-  leftForegroundAnim.pause();
-  rightForegroundAnim.pause();
-}
-function resumeAnimations() {
-  primaryPresentWiggle.resume(true);
-  leftForegroundAnim.resume(true);
-  rightForegroundAnim.resume(true);
-  card.reverse();
-  present.reverse();
-}
-ScrollTrigger.addEventListener("scrollStart", () => pauseAnimations());
-ScrollTrigger.addEventListener("scrollEnd", () => resumeAnimations());
-
-// ~*~*~*~*~*~*~*~ CLICK ANIMATIONS ~*~*~*~*~*~*~*~
-
-const primaryPresent = document.querySelector(".present-primary");
-primaryPresent.addEventListener("click", function (e) {
-  pauseAnimations();
-  if (present.reversed() && card.reversed()) {
-    present.play();
-    card.play();
-  } else {
-    present.reverse();
-    card.reverse();
-    resumeAnimations();
-  }
 });
